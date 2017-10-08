b7cab0d157c4f0bdfa9d2f132e88c71c6eaf3280
TVke <thomas.verhelst@student.kdg.be>
2017-10-08 22:35:50 +0200

    added player model

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Wedstrijd</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+</head>
+<body>
+@yield('content')
+</body>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'GameController@index')->name('game');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
