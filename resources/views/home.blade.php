733777874cbeb1953770e32a80149ae24ce268e3
TVke <thomas.verhelst@student.kdg.be>
2017-10-10 01:10:20 +0200

    added footer style and added new style classes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
 	<a class="button var-center var-call-to-action" href="{{ route('play') }}">{{ __('home.call-to-action') }}</a>
 	<section>
 		<h3 class="heading-2 var-underline">{{ __('home.winners-title') }}</h3>
-		<ol>
+		<ol class="list">
 			<li class="list-item">Thomas</li>
 		</ol>
 		<h3 class="heading-2 var-underline">{{ __('home.periods-title') }}</h3>
-		<ul>
+		<ul class="list">
 			<li class="list-item">{{ __('home.periods-body',['number'=>2,'start'=>"20/10/2017 00:00",'end'=>"29/10/2017 00:00"]) }}</li>
 			<li class="list-item">{{ __('home.periods-body',['number'=>3,'start'=>"20/11/2017 00:00",'end'=>"29/11/2017 00:00"]) }}</li>
 			<li class="list-item">{{ __('home.periods-body',['number'=>4,'start'=>"20/12/2017 00:00",'end'=>"29/12/2017 00:00"]) }}</li>
