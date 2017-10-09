04da45468e1444166cb24ed25b11bb77ef70a3ac
TVke <thomas.verhelst@student.kdg.be>
2017-10-09 15:19:11 +0200

    added scss basis and logo

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -2,4 +2,38 @@
 
 @section('content')
 <h1>Wedstrijd</h1>
+
+<div class="box neutral-0">
+
+</div>
+<div class="box neutral-1">
+
+</div>
+<div class="box neutral-2">
+
+</div>
+<div class="box neutral-3">
+
+</div>
+<div class="box neutral-4">
+
+</div>
+<div class="box neutral-5">
+
+</div>
+<div class="box neutral-6">
+
+</div>
+<div class="box neutral-7">
+
+</div>
+<div class="box neutral-8">
+
+</div>
+<div class="box neutral-9">
+
+</div>
+<div class="box neutral-10">
+
+</div>
 @endsection
