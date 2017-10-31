152f5d0a1981bb2648e2c1ac4d9031829b1e5f44
TVke <thomas.verhelst@student.kdg.be>
2017-10-31 12:53:16 +0100

    added dutch validations

--- database/seeds/PlayersTableSeeder.php
+++ database/seeds/PlayersTableSeeder.php
@@ -12,10 +12,8 @@ class PlayersTableSeeder extends Seeder
 	public function run()
 	{
 		DB::table('players')->insert([
-			['surname'=>'Verhelst','first_name'=>'Thomas','email'=>'tvke91@gmail.com','adres'=>'straatstraat 123','postalcode'=>'1234','city'=>'Kontich','ip'=>'127.0.0.1','start'=>null,'end'=>null,'time'=>'12343','possible_dis'=>'0','disqualified'=>'0','safety_token'=>'','friend_token'=>null,'friend_email'=>null,],
-			['surname'=>'Serrien','first_name'=>'Sam','email'=>'sam.serrien@kdg.be','adres'=>'Tramasantlei 122','postalcode'=>'1234','city'=>'Schoten','ip'=>'127.0.0.1','start'=>null,'end'=>null,'time'=>'8002','possible_dis'=>'0','disqualified'=>'0','safety_token'=>'','friend_token'=>null,'friend_email'=>null,],
-
-
+			['surname'=>'Verhelst','first_name'=>'Thomas','email'=>'tv@mail.com','adres'=>'straatstraat 123','postalcode'=>'1234','city'=>'Kontich','ip'=>'127.0.0.1','start'=>'1509445191.3570','end'=>'1509445234.4769','time'=>'418','possible_dis'=>'0','disqualified'=>'0','safety_token'=>'$2y$10$CpuoA3LFy8KVQilqZPQg/OFbotGrBfRDdMEk4hefKGiJta0vsnehi','friend_token'=>null,'friend_email'=>null,],
+			['surname'=>'Serrien','first_name'=>'Sam','email'=>'sam.se@mail.be','adres'=>'Tramasantlei 122','postalcode'=>'1234','city'=>'Schoten','ip'=>'127.0.0.1','start'=>'1509445737.0314','end'=>'1509445781.3445','time'=>'429','possible_dis'=>'0','disqualified'=>'0','safety_token'=>'','friend_token'=>null,'friend_email'=>null,],
 		]);
 	}
 }
